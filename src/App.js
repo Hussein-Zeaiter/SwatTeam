9823f6baed4b0f20604b5cc94e30c2fe5bfb9069
hussein-zeaiter <husseinztr03@gmail.com>
2024-09-08 20:16:53 +0300

    adding router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Router, Routes } from 'react-router-dom';
+import { Login } from './components/Login';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
